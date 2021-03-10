58ebedf359f5cd5f6ed3440a802317a20df639ee
Sergey <serhiopehota1991@gmail.com>
2021-03-10 21:58:34 +0300

    Connect Model to Controller

--- CSQuiz/QuestionsViewController.swift
+++ CSQuiz/QuestionsViewController.swift
@@ -9,22 +9,33 @@ import UIKit
 
 class Model {
     let question: String
+    let answers: [String]
     
-    init(question: String) {
+    init(question: String, answers: [String]) {
         self.question = question
+        self.answers = answers
     }
 }
-
 
 class QuestionsViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
    
-    let model = Model(question: "Перечислите основные принципы ООП")
+    @IBOutlet weak var firstButton: UIButton!
+    @IBOutlet weak var secondButton: UIButton!
+    @IBOutlet weak var thirdButton: UIButton!
+    @IBOutlet weak var fourthButton: UIButton!
+    
+    let model = Model(question: "Перечислите основные принципы ООП",
+                      answers: ["Полиморфизм", "Инкапсуляция", "Наследование", "Все выше перечисленное"])
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         titleLabel.text = model.question
+        
+        firstButton.setTitle(model.answers[0], for: .normal)
+        secondButton.setTitle(model.answers[1], for: .normal)
+        thirdButton.setTitle(model.answers[2], for: .normal)
+        fourthButton.setTitle(model.answers[3], for: .normal)
     }
-
 }
