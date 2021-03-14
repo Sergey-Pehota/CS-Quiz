6c765084bf80ba4db3a4aa1399fafe4443f32ce7
Sergey <serhiopehota1991@gmail.com>
2021-03-14 14:28:08 +0300

    Create QuizModel for QuizViewController and set data

--- CSQuiz/Source/QuizViewController.swift
+++ CSQuiz/Source/QuizViewController.swift
@@ -7,11 +7,23 @@
 
 import UIKit
 
+struct QuizModel {
+    let complexityTitles: [String]
+    let startTitle: String
+}
+
 class QuizViewController: UIViewController {
     @IBOutlet weak var complexitySegmentedControl: UISegmentedControl!
     @IBOutlet weak var startButton: UIButton!
     
+    let model = QuizModel(complexityTitles: ["Легко", "Сложно"], startTitle: "Начать")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        complexitySegmentedControl.setTitle(model.complexityTitles[0], forSegmentAt: 0)
+        complexitySegmentedControl.setTitle(model.complexityTitles[1], forSegmentAt: 1)
+        
+        startButton.setTitle(model.startTitle, for: .normal)
     }
 }
