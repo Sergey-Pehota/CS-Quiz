b4d050a5a3aa6f5f4b17817c0f18ec45aaa701b0
Sergey <serhiopehota1991@gmail.com>
2021-03-14 13:41:43 +0300

    Create QuizViewController with Outlets

---
+++ CSQuiz/Source/QuizViewController.swift
@@ -0,0 +1,17 @@
+//
+//  QuizViewController.swift
+//  CSQuiz
+//
+//  Created by Sergey on 14.03.21.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+    @IBOutlet weak var complexitySegmentedControl: UISegmentedControl!
+    @IBOutlet weak var startButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
