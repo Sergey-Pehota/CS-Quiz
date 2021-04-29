b273a0c0712cdee35dd798a1ad76bfe475878113
Sergey <serhiopehota1991@gmail.com>
2021-04-29 22:28:14 +0300

    Add array with questions and make finish view controller to app delegate

--- CSQuiz/Source/AppDelegate.swift
+++ CSQuiz/Source/AppDelegate.swift
@@ -11,6 +11,22 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     var quizViewController: QuizViewController!
+    var questionNavigationController: UINavigationController!
+    var index = 0
+    
+    var questions: [Question] = [
+        Question(
+            text: "Перечислите основные принципы ООП",
+            answers: ["Полиморфизм", "Инкапсуляция", "Наследование", "Все выше перечисленное"]),
+        
+        Question(
+            text: "Каким образом можно инициализировать массив?",
+            answers: ["1", "2", "3", "4"]),
+        
+        Question(
+            text: "Каким образом можно инициализировать массив?",
+            answers: ["5", "6", "7", "8"]),
+    ]
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
@@ -41,18 +57,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return navigationController
     }
     
-    func makeQuestionViewController() -> UIViewController {
-        let question = Question(
-            text: "Перечислите основные принципы ООП",
-            answers: ["Полиморфизм", "Инкапсуляция", "Наследование", "Все выше перечисленное"])
+    func makeQuestionViewController(question: Question) -> UIViewController {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let questionViewController = storyboard.instantiateViewController(identifier: "QuestionViewController") as! QuestionViewController
         questionViewController.delegate = self
         questionViewController.title = "1/10"
         questionViewController.question = question
-        let navigationController = UINavigationController(rootViewController: questionViewController)
 
-        return navigationController
+        return questionViewController
+    }
+    
+    func makeFinishViewController() -> UIViewController {
+        let vc = UIViewController()
+        vc.view.backgroundColor = .green
+        
+        return vc
     }
     
     func makeSettingsViewController() -> UIViewController {
@@ -70,13 +89,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 extension AppDelegate: QuizViewControllerDelegate {
     func didTapStartButton() {
-        let vc = makeQuestionViewController()
-        quizViewController.present(vc, animated: true, completion: nil)
+        let vc = makeQuestionViewController(question: questions[index])
+        questionNavigationController = UINavigationController(rootViewController: vc)
+        quizViewController.present(questionNavigationController, animated: true, completion: nil)
     }
 }
 
 extension AppDelegate: QuestionViewControllerDelegate {
     func didTapButton(option: String) {
-        print("Option: \(option)")
+        if index < questions.count - 1 {
+            index += 1
+            let vc = makeQuestionViewController(question: questions[index])
+            questionNavigationController.pushViewController(vc, animated: true)
+        } else {
+            let vc = makeFinishViewController()
+            questionNavigationController.pushViewController(vc, animated: true)
+        }
     }
 }
