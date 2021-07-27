bdaf2c366a6f92c3d0e1f16c03daa2d7d9840484
Nikita <nsdidenko.developer@gmail.com>
2021-07-27 22:21:54 +0300

    Add answer buttons programmatically

--- CSQuiz/Source/QuestionViewController.swift
+++ CSQuiz/Source/QuestionViewController.swift
@@ -19,32 +19,42 @@ protocol QuestionViewControllerDelegate {
 }
 
 class QuestionViewController: UIViewController {
-    @IBOutlet weak var titleLabel: UILabel!
-   
-    
     @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var answersStackView: UIStackView!
     
     var delegate: QuestionViewControllerDelegate?
     
     var question: Question!
     var progress: Float!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        titleLabel.text = question.text
-        
         progressView.setProgress(progress, animated: false)
-        
-        var questionButton = UIButton()
-//        questionButton.setTitle(question.answers[index], for: .normal)
-        questionButton.titleLabel?.font = .systemFont(ofSize: 24)
-        questionButton.titleLabel?.textColor = .systemBlue
-        questionButton.layer.cornerRadius = 12
-        questionButton.layer.borderWidth = 1
-        questionButton.layer.borderColor = UIColor.blue.cgColor
-        questionButton.backgroundColor = .systemGray5
-        questionButton.titleLabel?.adjustsFontSizeToFitWidth = true
+
+        titleLabel.text = question.text
+
+        question.answers.enumerated().forEach { index, answer in
+            let answerButton = UIButton()
+            answerButton.setTitle(answer, for: .normal)
+            answerButton.setTitleColor(.systemBlue, for: .normal)
+            answerButton.titleLabel?.font = .systemFont(ofSize: 24)
+            answerButton.layer.cornerRadius = 12
+            answerButton.layer.borderWidth = 1
+            answerButton.layer.borderColor = UIColor.blue.cgColor
+            answerButton.backgroundColor = .systemGray5
+            answerButton.titleLabel?.adjustsFontSizeToFitWidth = true
+            answerButton.tag = index
+            answerButton.addTarget(self, action: #selector(handleButtonTap(sender:)), for: .touchUpInside)
+
+            answersStackView.addArrangedSubview(answerButton)
+        }
+    }
+
+    @objc private func handleButtonTap(sender: UIButton) {
+        let chosenIndex = sender.tag
+        let chosenOption = question.answers[chosenIndex]
+        delegate?.didTapButton(chosenOption: chosenOption)
     }
 }
-
